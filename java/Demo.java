d9f96b2509f4c0572a5fdbd40ff430ae46f01b13
Waris Shaik <shaikwaris61@gmail.com>
2022-10-11 23:12:06 +0530

    updated

---
+++ java/Demo.java
@@ -0,0 +1,34 @@
+public class Demo{
+   public static void main(String[] args){
+
+		//Simple print "HELLO WORLD" java programm
+
+	   System.out.println("Hello World");
+         	   
+   }
+
+
+}
+
+
+//the word in first line called PUBLIC is a "access modifier" which means as name suggests public it is accessible from anywhere.
+
+
+
+//class means it contains set of properties such as methods functions main function and so on..
+
+
+//Demo means it is nothing but  a class Name
+
+
+// static means a method a reserved keyword which is used  with out creating an object of the main class(Demo)
+
+
+// void means nothing but it does return nothing
+
+
+
+//String[] args  these are belong to Strings theoratial and also called Command Line Arguments
+
+
+//
