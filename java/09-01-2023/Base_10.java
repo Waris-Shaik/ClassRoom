cdd77e2b50ac885ba39a451e201e7a55b4df0f3c
Waris Shaik <shaikwaris61@gmail.com>
2023-01-09 23:51:35 +0530

    updated

--- java/09-01-2023/Base_10.java
+++ java/09-01-2023/Base_10.java
@@ -5,5 +5,40 @@ public class Base_10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+// program to print the number as same as you have given n user input
+
+        System.out.print("Enter any medium Number: ");
+        int n = in.nextInt();
+        int ans = same_Number(n);
+        System.out.println(ans); // here we are printing ans not directly n you can observe...!
+        System.out.println("Done");
+
+
+
+    } static int same_Number(int num){
+        int ans = 0;
+        int count = 0;
+        while(num > 0){
+            int rem = num % 10;
+            num /= 10;
+            ans += rem * (Math.pow(10, count++));
+        }
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
