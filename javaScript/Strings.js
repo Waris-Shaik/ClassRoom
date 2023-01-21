fde635503803c0fb323395cafcd868597bb78fb8
Waris Shaik <shaikwaris61@gmail.com>
2023-01-21 23:27:18 +0530

    added

---
+++ javaScript/Strings.js
@@ -0,0 +1,13 @@
+// Strings indexing 
+
+// w a r i s
+// 0 1 2 3 4
+
+let firstName = "waris";
+console.log(firstName[0]); // w
+console.log(firstName[3]); // i
+
+// to find out the length of the firstName 
+// we use .length function 
+
+console.log(firstName.length);
