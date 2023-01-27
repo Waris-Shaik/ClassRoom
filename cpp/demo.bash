81eec0e515bc5ff43b4d28e464373d61cbf43ac4
Waris Shaik <shaikwaris61@gmail.com>
2023-01-28 00:07:42 +0530

    added

--- cpp/demo.bash
+++ cpp/demo.bash
@@ -1,1 +1,8 @@
-cd > for to change 
+#open cmd prompt and check what types of machines are available in your machine
+the command is:  wsl --list --online
+> pwd to check in which directory i am in
+> ls to see what directories are available
+> clear to remove all from home screen (temporary zone only none will be deleted).
+> ls to see all directories that are available
+> cd change directory u can go into a directory where u want.
+> cd .. | cd - to go back 
