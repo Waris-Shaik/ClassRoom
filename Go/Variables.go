f8689a7233fc4d777854505d6ffb972dfbf9e73a
Waris Shaik <shaikwaris61@gmail.com>
2023-03-19 00:32:45 +0530

    added

---
+++ Go/Variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var my_Name  string = "waris shaik";
+fmt.Println("The name is : ", my_Name);
+fmt.Printf("The type of the variable is : %T \n", my_Name);
+}
