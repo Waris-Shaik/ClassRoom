ce3ce1a61d90a928e364d69968927cbe71a64631
Waris Shaik <shaikwaris61@gmail.com>
2023-03-19 00:38:14 +0530

    updated

--- Go/Variables.go
+++ Go/Variables.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
+// how to get type of variable in go
+// %T is ued to get type of variables im go.
 
-var my_Name  string = "waris shaik";
-fmt.Println("The name is : ", my_Name);
-fmt.Printf("The type of the variable is : %T \n", my_Name);
+func main() {
+
+	var my_Name string = "waris shaik"
+	var my_Number int = 7
+	fmt.Println("The name is : ", my_Name)
+	fmt.Println("The Number is : ", my_Number)
+	fmt.Printf("The type of the variable is : %T \n", my_Name)   // %T is used to get to know which type it belongs to "STRING";
+	fmt.Printf("The type of the variable is : %T \n", my_Number) // int
 }
