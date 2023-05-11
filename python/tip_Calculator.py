1d16e9979b365b9a039975f07c3f07f2b1eac9bb
Waris Shaik <shaikwaris61@gmail.com>
2023-05-11 15:20:38 +0530

    updated

--- python/tip_Calculator.py
+++ python/tip_Calculator.py
@@ -1,5 +1,13 @@
-food_Amount = 100;
-tip_Percentage = 20 / 100;
-tip_Amount = food_Amount * tip_Percentage;
+food_Amount = float(input('Enter ypur food Amount $: '));
+tip_Msg = input('Enter how much tip u wanna give %: ');
+tip_Perc =  float(tip_Msg) / food_Amount;
+tip_Amount = food_Amount * tip_Perc;
 total_Bill = food_Amount + tip_Amount;
-print(total_Bill)
+print('total Amount is $: ',  total_Bill)
+print('Details are :')
+print('food Amount :', food_Amount);
+print('tip Given :',tip_Msg)
+print('calc Done :', tip_Perc);
+print('tip Aount :', tip_Amount);
+print('food Amount :', food_Amount);
+print('total Amount :', total_Bill)
